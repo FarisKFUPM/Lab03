6cb46cefd7ed34019df345909ac1ba91e2d1c59e
FarisKFUPM <fmszsh@gmail.com>
2024-02-05 16:32:12 +0300

    added Faris

--- Lab03.java
+++ Lab03.java
@@ -2,5 +2,6 @@ public class Lab03 {
     public static void main(String[] args) {
         System.out.println("Abdulmalek and faris");
         System.out.println("now");
+        System.out.println("Faris");
     }
 }
